386dee9d284d215af47be65399ead5bb2ad101fa
JonnyBoyHC <78315802+JonnyBoyHC@users.noreply.github.com>
2024-10-22 13:02:29 +0800

    added questions

--- python_while_loop_accum_ex_01.py
+++ python_while_loop_accum_ex_01.py
@@ -1,3 +1,14 @@
+"""
+Write a Python program using a while loop to calculate the sum of all
+odd numbers from 1 to 99 (i.e., 1, 3, 5, ..., 99).
+
+1. Use a while loop to iterate through the numbers.
+2. Accumulate the sum of all odd numbers between 1 and 99, inclusive.
+3. Print the final sum.
+The final sum number is 2500
+Save your file as "ClassNumber_YourName_Ex01.py"
+"""
+
 i = 1
 total = 0
 
--- python_while_loop_accum_ex_02.py
+++ python_while_loop_accum_ex_02.py
@@ -1,3 +1,15 @@
+"""
+Write a Python program using a while loop to calculate the product of
+all integers from 10 down to 1.
+
+1. Use a while loop to iterate through the numbers from 10 to 1.
+2. Accumulate the product of all these numbers.
+3. Print the final product.
+
+The final product number is 3628800
+Save your file as "ClassNumber_YourName_Ex02_py"
+"""
+
 i = 10
 product = 1
 

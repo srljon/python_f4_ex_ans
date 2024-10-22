541894bb21f6512948754a65348de8654b872923
JonnyBoyHC <78315802+JonnyBoyHC@users.noreply.github.com>
2024-10-22 12:57:51 +0800

    added loop accumulator

---
+++ python_while_loop_accum_ex_01.py
@@ -0,0 +1,8 @@
+i = 1
+total = 0
+
+while i <= 50:
+    total = total + (2*i - 1)
+    i = i + 1
+
+print(total)
---
+++ python_while_loop_accum_ex_02.py
@@ -0,0 +1,8 @@
+i = 10
+product = 1
+
+while i >= 1:
+    product = product * i
+    i = i - 1
+
+print(product)
